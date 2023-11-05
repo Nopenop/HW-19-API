ba7bee30c22f2796e8a315849f4df4457742b17b
Nopenop <ncampise2655@floridapoly.edu>
2023-11-05 09:46:34 -0500

    Finished draft

--- system_monitor.py
+++ system_monitor.py
@@ -1,33 +1,51 @@
 import tkinter as tk
 import requests
+import tools_config
 
-def button_press():
-    print("hello world")
-
-info = requests.get("http://127.0.0.1:8000").json()
+#configuration for information within loop
+route_info = tools_config.Path()
+info = requests.get(route_info.route).json()
 cpu = info["CPU"]
 memory = info["memory"]
 disk = info["disk"]
-# Create the main window
 root = tk.Tk()
+root.update()
+
+# Create the main window
 root.title("system monitor")
 
-# Create a label with the "Hello, World!" message
+#refreshes the information on the buttoms
+def refresh() -> None:
+    route_info = tools_config.Path()
+    info = requests.get(route_info.route).json()
+    cpu = info["CPU"]
+    memory = info["memory"]
+    disk = info["disk"]
+    cpu_info.config(text=f"CPU Usage %: {cpu}")
+    disk_info.config(text=f"Memory Usage %: {memory}")
+    mem_info.config(text=f"Disk Usage %: {disk}")
+
+#labels are global to configure upon refresh call
+global cpu_info
+global disk_info
+global mem_info
+
+# Creates a label with cpu usage
 cpu_info = tk.Label(root, text=f"CPU Usage %: {cpu}")
 cpu_info.pack(padx=20, pady=20)
-
+# Creates a label with disk usage
 disk_info = tk.Label(root, text=f"Disk Usage %: {memory}")
 disk_info.pack(padx=20, pady=20)
-
+# Creates a label with memory usage
 mem_info = tk.Label(root, text=f"Mem Usage %: {disk}")
 mem_info.pack(padx=20, pady=20)
 
-
-refresh_button = tk.Button(root, text="Refresh", command = button_press)
+#creates button that call refresh function
+refresh_button = tk.Button(root, text="Refresh", command = refresh)
 refresh_button.pack(padx=20, pady=20)
 
 
 
 
-# Start the tkinter main loop
+# Starts tkinter main loop
 root.mainloop()
